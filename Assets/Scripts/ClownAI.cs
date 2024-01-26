017493eeb90e622b1b9e50e4d8c339d42bf5e111
SeanyKay <sajkells@gmail.com>
2024-01-27 11:40:19 +1300

    Added sleepy clown and more level

--- Assets/Scripts/ClownAI.cs
+++ Assets/Scripts/ClownAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 // using UnityEditorInternal;
@@ -5,8 +6,14 @@ using UnityEngine;
 
 public class ClownAI : MonoBehaviour
 {
+    bool chasing = false;
+    AudioListener listener;
     public float moveSpeed = 1.0f;
     public float stoppingDistance = 0.01f;
+
+    //For use with an empty gameobject to quickly move our clown to the default starting chase position
+    private GameObject activeOrientationPlaceholder;
+    private Collider collider;
 
     private GameObject player;
     private Vector3 targetPosition;
@@ -14,16 +21,33 @@ public class ClownAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        collider = GetComponent<Collider>();
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!chasing) { return; }
         if (Vector3.Distance(transform.position, player.transform.position) > stoppingDistance)
         {
             transform.LookAt(player.transform);
             transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
+    }
+
+    //Triggered once player honks first time
+    private void AggroClown()
+    {
+        resetOrientation();
+        collider.enabled = true;
+        chasing = true;
+    }
+
+    //For use with an empty gameobject to quickly move our clown to the default starting chase position
+    private void resetOrientation()
+    {
+        transform.position = activeOrientationPlaceholder.transform.position;
+        transform.rotation = activeOrientationPlaceholder.transform.rotation;
     }
 }
