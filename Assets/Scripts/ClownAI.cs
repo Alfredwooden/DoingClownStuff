f5eedc241d08e0777355ea738cdb3276e32b86ae
TheUsualCam <cameron.g.rooney@gmail.com>
2024-01-27 00:07:30 +1300

    Simple Clown Chasing AI
    Clown chases player and stops when they touch. Can adjust speed via moveSpeed variable in editor on ClownAI.cs

---
+++ Assets/Scripts/ClownAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class ClownAI : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float stoppingDistance = 0.01f;
+
+    private GameObject player;
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) > stoppingDistance)
+        {
+            transform.LookAt(player.transform);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+}
